ba3e70a679c5aee0c495b664abd3e406d6626024
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-11-14 11:43:24 +0100

    Initializing Spring

---
+++ qualitag/src/test/java/it/unisannio/studenti/qualitag/QualitagApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisannio.studenti.qualitag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
