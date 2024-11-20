11c8c2824872bf51567f21bb99ca7e77916d12f7
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-11-20 19:08:11 +0100

    Initialized service dir

---
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/service/UserService.java
@@ -0,0 +1,8 @@
+package it.unisannio.studenti.qualitag.service;
+
+/**
+ * The userService class is a service class that provides methods to manage the user entity.
+ */
+public class UserService {
+
+}
