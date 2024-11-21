d394adc2b9e49c83a4bf2b23823e23f114ab7203
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-11-21 17:58:52 +0100

    Created repository interface for user extending MongoRepository. Contains methods to check if a user exists, finds it and deletes it given either the username or the email.

---
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/repository/UserRepository.java
@@ -0,0 +1,55 @@
+package it.unisannio.studenti.qualitag.repository;
+
+import it.unisannio.studenti.qualitag.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Repository for the User entity.
+ */
+public interface UserRepository extends MongoRepository<User, String> {
+  /**
+   * Checks if a user with the given username exists.
+   *
+   * @param username The username to check.
+   * @return True if a user with the given username exists, false otherwise.
+   */
+  Boolean existsByUsername(String username);
+
+  /**
+   * Finds a user by its username.
+   *
+   * @param username The username of the user to find.
+   * @return User with the given username.
+   */
+  User findByUsername(String username);
+
+  /**
+   * Deletes a user by its username.
+   *
+   * @param username The username of the user to delete.
+   */
+  void deleteByUsername(String username);
+
+  /**
+   * Checks if a user with the given email exists.
+   *
+   * @param email The email to check.
+   * @return True if a user with the given email exists, false otherwise.
+   */
+  Boolean existsByEmail(String email);
+
+  /**
+   * Finds a user by its email.
+   *
+   * @param email The email of the user to find.
+   * @return User with the given email.
+   */
+  User findByEmail(String email);
+
+  /**
+   * Deletes a user by its email.
+   *
+   * @param email The email of the user to delete.
+   */
+  void deleteByEmail(String email);
+}
