e8630808ee67a3087d477437bebe493813cd5f21
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2024-12-01 22:50:17 +0100

    trying another checktyle configuration

--- src/main/java/it/unisannio/studenti/qualitag/QualitagApplication.java
+++ src/main/java/it/unisannio/studenti/qualitag/QualitagApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QualitagApplication {
 
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(QualitagApplication.class, args);
 	}
