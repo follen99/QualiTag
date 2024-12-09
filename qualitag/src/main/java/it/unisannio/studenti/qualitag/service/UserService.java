8bd7ee814d6c415b8f9c6c455f07be54e010e224
Mattia Marino <mattia.marino01@gmail.com>
2024-12-09 18:57:26 +0100

    Bugfix

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/service/UserService.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/service/UserService.java
@@ -179,7 +179,7 @@ public class UserService {
     User user = userRepository.findByUsername(username);
     if (user == null) {
       response.put("msg", "User not found.");
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     // TODO: Update to use DTOs
