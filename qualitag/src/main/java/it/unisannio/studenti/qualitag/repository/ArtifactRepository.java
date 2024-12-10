5f05324e9a9fbf036ee75d455c345025b7074e16
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-12-10 12:28:55 +0100

    ArtifactRepository reformat to meet Google Java style

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/repository/ArtifactRepository.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/repository/ArtifactRepository.java
@@ -3,17 +3,16 @@ package it.unisannio.studenti.qualitag.repository;
 import it.unisannio.studenti.qualitag.model.Artifact;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
+/**
+ * Repository for the Artifact entity. Provides methods to interact with the database.
+ */
 public interface ArtifactRepository extends MongoRepository<Artifact, String> {
 
   /**
-   * Finds an artifact by its id
+   * Finds an artifact by its id.
    *
    * @param artifactId the id of the artifact to find
    * @return the artifact with the given id
    */
   Artifact findArtifactByArtifactId(String artifactId);
-
-
 }
