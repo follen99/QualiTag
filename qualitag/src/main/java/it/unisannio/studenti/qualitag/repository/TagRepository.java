6456f2378924d997c112855c5e0adb4f808dc604
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-12-10 14:18:24 +0100

    TagRepository reformat to meet Google Java style

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/repository/TagRepository.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/repository/TagRepository.java
@@ -4,13 +4,24 @@ import it.unisannio.studenti.qualitag.model.Tag;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/**
+ * Repository interface for managing Tag entities in MongoDB.
+ */
 public interface TagRepository extends MongoRepository<Tag, String> {
 
-  //List<Tag> findByCreatedBy(String createdBy);
+  /**
+   * Finds tags created by a specific user.
+   *
+   * @param value The username of the creator.
+   * @return A list of tags created by the specified user.
+   */
   List<Tag> findTagByCreatedBy(String value);     // created by username
 
-
+  /**
+   * Finds tags that contain the specified value.
+   *
+   * @param value The value to search for within tag values.
+   * @return A list of tags containing the specified value.
+   */
   List<Tag> findByTagValueContaining(String value);
-
-
 }
