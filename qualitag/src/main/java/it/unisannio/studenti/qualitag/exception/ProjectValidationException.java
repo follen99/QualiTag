0649c9d4c45ba119fb35ff2ccad2e899b156ca2e
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-12-10 14:06:28 +0100

    ProjectValidationException and TagConstants reformat to meet Google Java style

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/exception/ProjectValidationException.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/exception/ProjectValidationException.java
@@ -1,12 +1,16 @@
 package it.unisannio.studenti.qualitag.exception;
 
+/**
+ * ProjectValidationException is a custom exception class that extends RuntimeException.
+ * It is used to handle exceptions related to project validation.
+ */
 public class ProjectValidationException extends RuntimeException {
 
   /**
    * Constructs a new ProjectValidationException.
    * Used to handle exceptions related to project validation.
    *
-   * @param message
+   * @param message the message to be displayed when the exception is thrown
    */
   public ProjectValidationException(String message) {
     super(message);
