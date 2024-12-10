6e7a5267e268d19d644ce57176a3ac8ec3af2a61
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-12-10 12:49:15 +0100

    ProjectRepository reformat to meet Google Java style

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/repository/ProjectRepository.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/repository/ProjectRepository.java
@@ -4,21 +4,40 @@ import it.unisannio.studenti.qualitag.model.Project;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/**
+ * Repository for the Project model.
+ */
 public interface ProjectRepository extends MongoRepository<Project, String> {
 
   /**
-   * Finds a project by its id
+   * Finds a project by its id.
    *
    * @param projectId The id of the project to find
    * @return The project with the given id
    */
   Project findProjectByProjectId(String projectId);
 
-  //checks if a project created by a user with id ownerId exists
+  /**
+   * Checks if a project with the given owner id exists.
+   *
+   * @param ownerId The id of the owner of the project to check
+   * @return True if a project with the given id exists, false otherwise
+   */
   boolean existsByOwnerId(String ownerId);
 
-  //finds all the projects created by a user with id ownerId
+  /**
+   * Finds all projects with the given owner id.
+   *
+   * @param ownerId The id of the owner of the projects to find
+   * @return A list of projects with the given owner id
+   */
   List<Project> findProjectsByOwnerId(String ownerId);
 
+  /**
+   * Checks if a project with the given name exists.
+   *
+   * @param name The name of the project to check
+   * @return True if a project with the given name exists, false otherwise
+   */
   boolean existsByProjectName(String name);
 }
