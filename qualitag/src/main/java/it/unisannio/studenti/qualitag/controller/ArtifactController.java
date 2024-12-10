a29a195165c3f444ae81f893a04a9c4561eeab16
Mattia Marino <128101085+Mattia-Marino@users.noreply.github.com>
2024-12-10 12:26:08 +0100

    ArtifactController reformat to meet Google Java style

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/controller/ArtifactController.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/controller/ArtifactController.java
@@ -4,8 +4,18 @@ import it.unisannio.studenti.qualitag.dto.artifact.ArtifactCreateDto;
 import it.unisannio.studenti.qualitag.service.ArtifactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The controller for the artifact endpoints.
+ */
 @RestController
 @RequestMapping("/api/v1/artifacts")
 public class ArtifactController {
@@ -13,7 +23,7 @@ public class ArtifactController {
   private final ArtifactService artifactService;
 
   /**
-   * Constructs a new ArtifactController
+   * Constructs a new ArtifactController.
    *
    * @param artifactService the artifact service
    */
@@ -22,10 +32,10 @@ public class ArtifactController {
     this.artifactService = artifactService;
   }
 
+  // POST Methods
 
-//POST
   /**
-   * Adds an artifact to the repository
+   * Adds an artifact to the repository.
    *
    * @param artifactCreateDto the artifact to add to the repository
    */
@@ -35,9 +45,10 @@ public class ArtifactController {
   }
 
   /**
-   * Adds a tag to an artifact
+   * Adds a tag to an artifact.
+   *
    * @param artifactId the id of the artifact
-   * @param tagId the id of the tag
+   * @param tagId      the id of the tag
    * @return the response entity
    */
   @PostMapping("/add/{artifactId}/tag/{tagId}")
@@ -45,9 +56,10 @@ public class ArtifactController {
     return this.artifactService.addTag(artifactId, tagId);
   }
 
-//GET
+  // GET Methods
+
   /**
-   * Gets all the artifacts
+   * Gets all the artifacts.
    *
    * @return the response entity
    */
@@ -56,22 +68,25 @@ public class ArtifactController {
     return artifactService.getAllArtifacts();
   }
 
- //PUT
+  // PUT Methods
+
   /**
-   * Updates an artifact
-   * @param artifactId the id of the artifact to update
+   * Updates an artifact.
+   *
+   * @param artifactId        the id of the artifact to update
    * @param artifactCreateDto the updated artifact
    * @return the response entity
-    */
+   */
   @PutMapping("/update/{artifactId}")
-    public ResponseEntity<?> updateArtifact(@PathVariable String artifactId, @RequestBody ArtifactCreateDto artifactCreateDto) {
-      return this.artifactService.updateArtifact(artifactCreateDto, artifactId);
-    }
+  public ResponseEntity<?> updateArtifact(@PathVariable String artifactId,
+      @RequestBody ArtifactCreateDto artifactCreateDto) {
+    return this.artifactService.updateArtifact(artifactCreateDto, artifactId);
+  }
 
+  // DELETE Methods
 
-//DELETE
   /**
-   * Deletes an artifact from the repository
+   * Deletes an artifact from the repository.
    *
    * @param artifactId the id of the artifact to delete
    */
@@ -81,9 +96,10 @@ public class ArtifactController {
   }
 
   /**
-   * Deletes the associated tag
+   * Deletes the associated tag.
+   *
    * @param artifactId the id of the artifact to delete the tag from
-   * @param tagId the id of the tag to delete
+   * @param tagId      the id of the tag to delete
    * @return the response entity
    */
   @DeleteMapping("/delete/{artifactId}/tag/{tagId}")
