c6493ebd91c66c6ca7f59d15975a469d111654c4
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2024-12-11 22:22:31 +0100

    checkstyle fix

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/view/ProjectViewController.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/view/ProjectViewController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * The ProjectViewController class is a controller
+ * that handles requests related to the projects view.
+ */
 @Controller
 @RequestMapping("/project")
 public class ProjectViewController {
