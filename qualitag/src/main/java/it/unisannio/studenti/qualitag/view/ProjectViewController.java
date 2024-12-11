42e3aa795108e98fc2be224cb91a53c6d41ebc78
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2024-12-11 22:19:00 +0100

    working base projectdetails

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/view/ProjectViewController.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/view/ProjectViewController.java
@@ -10,9 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/project")
 public class ProjectViewController {
 
+  /**
+   * Returns the projects view.
+   *
+   * @param username The username of the user.
+   * @param model    The model.
+   * @return the projects view
+   */
   @GetMapping("/{username}/projects")
   public String myProjects(@PathVariable("username") String username, Model model) {
     model.addAttribute("username", username);
     return "project/my_projects";
+  }
+
+  /**
+   * Returns the project details view.
+   *
+   * @param projectId The id of the project.
+   * @param model     The model.
+   * @return the project details view
+   */
+  @GetMapping("/detail/{projectId}")
+  public String project(@PathVariable("projectId") String projectId, Model model) {
+    model.addAttribute("projectId", projectId);
+    return "project/project_details";
   }
 }
