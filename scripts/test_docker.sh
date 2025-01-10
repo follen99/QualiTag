e520aeefc6c97e9089f3c3bbecb3be6ae908bcd4
Mattia Marino <mattia.marino01@gmail.com>
2025-01-10 10:07:51 +0100

    Updates on docker test script

--- scripts/test_docker.sh
+++ scripts/test_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 # Make the script executable
 chmod +x "$0"
 
@@ -7,13 +10,17 @@ chmod +x "$0"
 cd ..
 
 # Build the Docker container
+echo -e "Building the Docker container..."
 docker compose build
 
 # Run the Docker container
+echo -e "\n\nStarting the Docker container..."
 docker compose up -d
 
 # Run the tests
+echo -e "\n\nRunning the tests..."
 docker compose exec -T qualitag ./gradlew test
 
 # Stop the Docker container
+echo -e "\n\nStopping the Docker container..."
 docker compose stop
