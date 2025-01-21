b1d862406bd23355184e216f5bbf58e9480ca4de
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2025-01-21 20:39:27 +0100

    reusing modal in progress

--- qualitag/src/main/resources/static/js/auth/sign-up-script.js
+++ qualitag/src/main/resources/static/js/auth/sign-up-script.js
@@ -1,10 +1,15 @@
+import {LoadingModal} from "../modals/loading-modal.js";
+
 document.getElementById('registrationForm').addEventListener('submit',
     async function (event) {
       event.preventDefault();
 
+      const loadingModal = new LoadingModal("/", "Registering user...");
+
       const formData = new FormData(event.target);
       const data = Object.fromEntries(formData.entries());
-      console.log(JSON.stringify(data));
+
+      loadingModal.showModal(true);
       try {
         // TODO: if mail system does not work, tell to user
         const response = await fetch('/api/v1/auth/register', {
@@ -19,12 +24,15 @@ document.getElementById('registrationForm').addEventListener('submit',
           // Success: Use the token from the JSON response
           localStorage.setItem('authToken', responseData.token);
           localStorage.setItem('username', responseData.username);
-          alert(responseData.msg || 'Registration successful');
-          window.location.href = '/'; // Redirect to homepage after registration
+          // alert(responseData.msg || 'Registration successful');
+          // window.location.href = '/'; // Redirect to homepage after registration
+
+          loadingModal.showModal("Registration successful!", false, true, true);
         } else {
           // Error: Display the error message from the JSON response
-          alert(
-              'Registration failed: ' + (responseData.msg || 'Unknown error'));
+          loadingModal.showModal(
+              "Something went wrong!\n Message: " + responseData.msg
+              , false, true, true);
         }
       } catch (error) {
         alert('An error occurred: ' + error.message);
