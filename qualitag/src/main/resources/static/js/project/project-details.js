19d0277bee796a898621d2e682d5d6b03d48f9f6
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2025-01-23 19:42:21 +0100

    reusable code to display list inside a dropdown

--- qualitag/src/main/resources/static/js/project/project-details.js
+++ qualitag/src/main/resources/static/js/project/project-details.js
@@ -1,4 +1,21 @@
 let globalProjectData;
+const artifactsIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">
+                      <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                      <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
+                    </svg>`;
+
+const usersIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                        </svg>`;
+
+const teamIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-microsoft-teams" viewBox="0 0 16 16">
+                          <path d="M9.186 4.797a2.42 2.42 0 1 0-2.86-2.448h1.178c.929 0 1.682.753 1.682 1.682zm-4.295 7.738h2.613c.929 0 1.682-.753 1.682-1.682V5.58h2.783a.7.7 0 0 1 .682.716v4.294a4.197 4.197 0 0 1-4.093 4.293c-1.618-.04-3-.99-3.667-2.35Zm10.737-9.372a1.674 1.674 0 1 1-3.349 0 1.674 1.674 0 0 1 3.349 0m-2.238 9.488-.12-.002a5.2 5.2 0 0 0 .381-2.07V6.306a1.7 1.7 0 0 0-.15-.725h1.792c.39 0 .707.317.707.707v3.765a2.6 2.6 0 0 1-2.598 2.598z"/>
+                          <path d="M.682 3.349h6.822c.377 0 .682.305.682.682v6.822a.68.68 0 0 1-.682.682H.682A.68.68 0 0 1 0 10.853V4.03c0-.377.305-.682.682-.682Zm5.206 2.596v-.72h-3.59v.72h1.357V9.66h.87V5.945z"/>
+                        </svg>`;
+
+const defaultIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-app" viewBox="0 0 16 16">\n'
+    + '  <path d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4z"/>\n'
+    + '</svg>'
 
 document.addEventListener('DOMContentLoaded', function () {
   // Token validation
@@ -23,7 +40,8 @@ document.addEventListener('DOMContentLoaded', function () {
         // get error message
         console.log("Error message: " + errorData.msg);
         alert(errorData.msg + "\nYou will redirect to the project list page.");
-        window.location.href = "/project/" + localStorage.getItem('username') + "/projects";
+        window.location.href = "/project/" + localStorage.getItem('username')
+            + "/projects";
       });
     }
     return response.json(); // Se la risposta è ok, continua con il parsing dei dati
@@ -45,9 +63,11 @@ document.addEventListener('DOMContentLoaded', function () {
      * "teams":["6753782aebf3c97069ad69a4"]}
      */
 
-    document.getElementById('projectName').innerText = "Name: " + project.projectName;
+    document.getElementById('projectName').innerText = "Name: "
+        + project.projectName;
     document.getElementById(
-        'projectDescription').innerText = "Description: " + project.projectDescription;
+        'projectDescription').innerText = "Description: "
+        + project.projectDescription;
     document.getElementById(
         'projectDeadline').innerText = `Deadline: ${new Date(
         project.projectDeadline).toLocaleDateString()}`;
@@ -63,87 +83,26 @@ document.addEventListener('DOMContentLoaded', function () {
         '#dropdownMenuButtonUsers + .dropdown-menu');
     usersDropdown.innerHTML = ''; // Clear existing items
 
-    project.users.forEach(user => {
-      // Creating an element <li> for each user
-      const userItem = document.createElement('li');
+    if (project.users.length === 0) {
+      const noUser = {username: "No users Available"};
+      showElementsInsideDropdown(usersDropdown, [noUser], false,
+          '', '', '', defaultIcon, 'username');
+    }
 
-      // Creating SVG icon
-      const icon = document.createElement('span');
-      icon.innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                        </svg>
-                    `;
-      icon.style.marginLeft = '1em';
-
-      // Adding SVG icon to <li>
-      userItem.appendChild(icon);
-
-      // Adding username
-      const username = document.createElement('span');
-      username.textContent = user.username;
-      username.style.marginLeft = '0.3em';
-
-      // adding username to <li>
-      userItem.appendChild(username);
-
-      // adding <li> to dropdown
-      usersDropdown.appendChild(userItem);
-    });
+    showElementsInsideDropdown(usersDropdown, project.users, false, '', '', '', usersIcon, 'username');
 
     // adding team list
     const teamsDropdown = document.querySelector(
         '#dropdownMenuButtonTeams + .dropdown-menu');
     teamsDropdown.innerHTML = ''; // Clear existing items
 
-    project.teams.forEach(team => {
-      // Creating an element <li> for each team
+    if (project.teams.length === 0) {
+      const noTeam = {teamName: "No teams Available"};
+      showElementsInsideDropdown(teamsDropdown, [noTeam], false,
+          '', '', '', defaultIcon, 'teamName');
+    }
 
-      const teamItem = document.createElement('li');
-
-      // using a button so that the user can click on the team to see the details
-      // making the button look like a list item
-      const teamButton = document.createElement('button');
-      teamButton.style.display = 'list-item';
-      teamButton.style.listStyleType = 'none';
-      teamButton.style.padding = '0';
-      teamButton.style.margin = '0';
-      teamButton.style.border = 'none';
-      teamButton.style.background = 'none';
-      teamButton.style.textAlign = 'left';
-      teamButton.style.width = '100%';
-      teamButton.style.color = 'white';
-
-      // Creating SVG icon
-      const icon = document.createElement('span');
-      icon.innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-microsoft-teams" viewBox="0 0 16 16">
-                          <path d="M9.186 4.797a2.42 2.42 0 1 0-2.86-2.448h1.178c.929 0 1.682.753 1.682 1.682zm-4.295 7.738h2.613c.929 0 1.682-.753 1.682-1.682V5.58h2.783a.7.7 0 0 1 .682.716v4.294a4.197 4.197 0 0 1-4.093 4.293c-1.618-.04-3-.99-3.667-2.35Zm10.737-9.372a1.674 1.674 0 1 1-3.349 0 1.674 1.674 0 0 1 3.349 0m-2.238 9.488-.12-.002a5.2 5.2 0 0 0 .381-2.07V6.306a1.7 1.7 0 0 0-.15-.725h1.792c.39 0 .707.317.707.707v3.765a2.6 2.6 0 0 1-2.598 2.598z"/>
-                          <path d="M.682 3.349h6.822c.377 0 .682.305.682.682v6.822a.68.68 0 0 1-.682.682H.682A.68.68 0 0 1 0 10.853V4.03c0-.377.305-.682.682-.682Zm5.206 2.596v-.72h-3.59v.72h1.357V9.66h.87V5.945z"/>
-                        </svg>
-                    `;
-      icon.style.marginLeft = '1em';
-
-      // Adding SVG icon to <li>
-      teamButton.appendChild(icon);
-
-      // Adding team
-      const teamName = document.createElement('span');
-      teamName.textContent = team.teamName;
-      teamName.style.marginLeft = '0.3em';
-
-      // adding teamName to <li>
-      teamButton.appendChild(teamName);
-      teamItem.appendChild(teamButton);
-
-      // adding <li> to dropdown
-      teamsDropdown.appendChild(teamItem);
-
-
-      teamButton.addEventListener('click', () => {
-        window.location.href = `/team/${team.teamId}/details`;
-      });
-    });
+    showElementsInsideDropdown(teamsDropdown, project.teams, true, '/team/', '/details', 'teamId', teamIcon, 'teamName');
 
     // adding team list
     const artifactsDropdown = document.querySelector(
@@ -151,10 +110,19 @@ document.addEventListener('DOMContentLoaded', function () {
     artifactsDropdown.innerHTML = ''; // Clear existing items
 
     if (project.artifacts.length === 0) {
-      const noArtifact = { artifactName: "No artifacts Available" };
-      showTeamInsideDropdown(artifactsDropdown,[noArtifact], false);
-    }else {
-      showTeamInsideDropdown(artifactsDropdown, project.artifacts);
+      const noArtifact = {artifactName: "No artifacts Available"};
+      showElementsInsideDropdown(artifactsDropdown, [noArtifact], false,
+          '', '', '', defaultIcon, 'artifactName');
+    } else {
+      console.log("eccomi qui");
+      showElementsInsideDropdown(artifactsDropdown,
+          project.artifacts,
+          true,
+          '/artifact/',
+          '/tag',
+          'artifactId',
+          artifactsIcon,
+          'artifactName');
     }
 
   })
@@ -174,39 +142,61 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-function showTeamInsideDropdown(artifactsDropdown, artifacts, clickable = true) {
-  artifacts.forEach(artifact => {
+/**
+ * Show elements inside a dropdown.
+ *
+ * @param targetDropdown the dropdown where the elements will be shown
+ * @param list the list of elements to show
+ * @param clickable if the elements are clickable
+ * @param headerUrl the url to append before the id
+ * @param footerUrl the url to append after the id
+ * @param itemIdName the name of the id attribute
+ * @param icon the icon to show before the text
+ * @param attributeName the name of the attribute to show inside the dropdown
+ */
+function showElementsInsideDropdown(targetDropdown,
+    list,
+    clickable = false,
+    headerUrl,
+    footerUrl,
+    itemIdName,
+    icon = defaultIcon,
+    attributeName
+) {
+  list.forEach(listItem => {
     // Creating an element <li> for each team
-    const teamItem = document.createElement('li');
+    const wrapperItem = document.createElement('li');
+
+    // Creating the <a> element
+    const link = document.createElement('a');
+    if (clickable) {
+      link.href = headerUrl + listItem[itemIdName] + footerUrl;
+    }
+
+    link.style.textDecoration = 'none';
+    link.style.color = 'inherit'; // To inherit the color from the parent
 
     // Creating SVG icon
-    const icon = document.createElement('span');
-    icon.innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">
-                          <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
-                          <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
-                        </svg>
-                    `;
-    icon.style.marginLeft = '1em';
+    const iconElement = document.createElement('span');
+    iconElement.innerHTML = icon;
+    iconElement.style.marginLeft = '1em';
+    iconElement.style.marginRight = '1em';
 
-    // Adding SVG icon to <li>
-    teamItem.appendChild(icon);
+    // Adding SVG icon to <a>
+    link.appendChild(iconElement);
 
     // Adding team
-    const artifactName = document.createElement('span');
-    artifactName.textContent = artifact.artifactName;
-    artifactName.style.marginLeft = '0.3em';
+    const textToShow = document.createElement('span');
+    textToShow.textContent = listItem[attributeName];
+    textToShow.style.marginLeft = '0.3em';
 
-    // adding artifactName to <li>
-    teamItem.appendChild(artifactName);
+    // Adding artifactName to <a>
+    link.appendChild(textToShow);
 
-    // adding <li> to dropdown
-    artifactsDropdown.appendChild(teamItem);
+    // Adding <a> to <li>
+    wrapperItem.appendChild(link);
 
-    if (clickable) {
-      teamItem.addEventListener('click', () => {
-        window.location.href = `/artifact/${artifact.artifactId}/tag`;
-      });
-    }
+    // Adding <li> to dropdown
+    targetDropdown.appendChild(wrapperItem);
   });
 }
