a094b207bff57fac7a10851b417eb7f5df96d02a
Mattia Marino <mattia.marino01@gmail.com>
2025-02-15 17:10:53 +0100

    More checkstyle corrections and test on health controller

--- qualitag/src/main/java/it/unisannio/studenti/qualitag/controller/HealthController.java
+++ qualitag/src/main/java/it/unisannio/studenti/qualitag/controller/HealthController.java
@@ -7,14 +7,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This class is used to check the health of the server.
+ */
 @RestController
 public class HealthController {
-    
-    @GetMapping("/ping")
-    public ResponseEntity<?> ping() {
-        Map<String, String> response = new HashMap<>();
 
-        response.put("msg", "Server is up and running");
-        return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
+  /**
+   * Checks if the server is up and running.
+   *
+   * @return a response entity with the message "Server is up and running"
+   */
+  @GetMapping("/ping")
+  public ResponseEntity<?> ping() {
+    Map<String, String> response = new HashMap<>();
+
+    response.put("msg", "Server is up and running");
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
 }
